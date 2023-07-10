f1d95f1167349bf4a893870158f4a0a7978e44b4
R-o-s-e-B <134575747+R-o-s-e-B@users.noreply.github.com>
2023-07-10 11:54:42 +0530

    Colorthief again

--- main.py
+++ main.py
@@ -3,39 +3,40 @@ from werkzeug.utils import secure_filename
 import os
 from colorthief import ColorThief
 from random import randrange
-import numpy as np
-from PIL import Image, ImageOps
+
 top_colors = None
 
 hex_list = []
 filename = ""
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = "static/images/"
+app.secret_key = "secret key"
 
-def rgb_to_hex(rgb):
-    return '%02x%02x%02x' % rgb
-def give_colors(file_path):
-    hex_list.clear()
-    my_image = Image.open(file_path).convert('RGB')
-    image_array = np.array(my_image)
-    unique_colors = {}  # (r, g, b): count
-    for column in image_array:
-        for rgb in column:
-            t_rgb = tuple(rgb)
-            if t_rgb not in unique_colors:
-                unique_colors[t_rgb] = 0
-            if t_rgb in unique_colors:
-                unique_colors[t_rgb] += 1
-    sorted_unique_colors = sorted(
-        unique_colors.items(), key=lambda x: x[1],
-        reverse=True)
-    converted_dict = dict(sorted_unique_colors)
-    values = list(converted_dict.keys())
-    top_10 = values[0:10]
-
-    for key in top_10:
-        hex = rgb_to_hex(key)
-        hex_list.append(f"#{hex}")
+# def rgb_to_hex(rgb):
+#     return '%02x%02x%02x' % rgb
+# def give_colors(file_path):
+#     hex_list.clear()
+#     my_image = Image.open(file_path).convert('RGB')
+#     image_array = np.array(my_image)
+#     unique_colors = {}  # (r, g, b): count
+#     for column in image_array:
+#         for rgb in column:
+#             t_rgb = tuple(rgb)
+#             if t_rgb not in unique_colors:
+#                 unique_colors[t_rgb] = 0
+#             if t_rgb in unique_colors:
+#                 unique_colors[t_rgb] += 1
+#             print(unique_colors)
+#     sorted_unique_colors = sorted(
+#         unique_colors.items(), key=lambda x: x[1],
+#         reverse=True)
+#     converted_dict = dict(sorted_unique_colors)
+#     values = list(converted_dict.keys())
+#     top_10 = values[0:10]
+#
+#     for key in top_10:
+#         hex = rgb_to_hex(key)
+#         hex_list.append(f"#{hex}")
 
 @app.route('/')
 def home():
@@ -53,16 +54,15 @@ def upload():
 
     if request.method == 'POST':
         file = request.files['file']
-        give_colors(file)
-        # filename = secure_filename(file.filename)
-        # file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        # full_path = f"static/images/{filename}"
-        # colors = ColorThief(filename)
-        # top_colors = colors.get_palette(color_count=10)
-        # hex_list = []
-        # for i in top_colors:
-        #
-        #     hex_list.append('#'+rgb_to_hex(i))
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        full_path = f"static/images/{filename}"
+        colors = ColorThief(full_path)
+        top_colors = colors.get_palette(color_count=10)
+        hex_list = []
+        for i in top_colors:
+
+            hex_list.append('#'+rgb_to_hex(i))
 
         return redirect(url_for('home'))
 
